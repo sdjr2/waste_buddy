11dea9ac32eb00a12706f8e651764353a26800da
Divanshu Sharma <sharma.divanshu30@gmail.com>
2020-03-11 04:09:16 -0700

    login page and main page updated

---
+++ scripts/firebase_api_wasteBuddy.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCxVIaMGqHY6U-5cdZy64DoYhHHIDXXusc",
+    authDomain: "wastebuddy-10c2d.firebaseapp.com",
+    databaseURL: "https://wastebuddy-10c2d.firebaseio.com",
+    projectId: "wastebuddy-10c2d",
+    storageBucket: "wastebuddy-10c2d.appspot.com",
+    messagingSenderId: "302385812152",
+    appId: "1:302385812152:web:8918c01855bf3df0651a4d",
+    measurementId: "G-J7072CLH0N"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
