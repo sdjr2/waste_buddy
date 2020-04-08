c6892c8a49db2e262839e36c793bb296bc3d1781
Ernest Wong <ernest.kinman.wong@gmail.com>
2020-04-08 04:37:25 -0700

    FIXXED

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,9 @@
 let items = ["itemImage1", "itemImage2", "itemImage3", "itemImage4", "itemImage5", "itemImage6", "itemImage7", "itemImage8", "itemImage9", "itemImage10", "itemImage11"];
 let itemName = ["Item: null", "Item: coffee cup", "Item: milk container", "Item: fish", "Item: fruits", "Item: paper plate", "Item: juice box", "Item: pop can", "Item: water bottle", "Item: newsprint", "Item: books"];
 
+let score = 0;
+let life = 7;
+
 // Randomizes words
 let chosenItem = Math.floor(Math.random() * items.length);
 
@@ -26,8 +29,12 @@ function randomItem() {
 function checkRecyclables() {
     if (chosenItem == 1 || chosenItem == 2) {
         console.log("WIN");
+        score++;
     } else {
         console.log("DIDN'T YOUR MAMMA TEACH YOU HOW TO RECYCLE???");
+        score--;
+        life--;
+        death();
     }
     document.getElementById(items[chosenItem]).style.display = "none";
     randomItem();
@@ -36,8 +43,12 @@ function checkRecyclables() {
 function checkCompost() {
     if (chosenItem == 3 || chosenItem == 4 || chosenItem == 5) {
         console.log("DEVU IS PROUD");
+        score++;
     } else {
         console.log("GO HOME BRO");
+        score--;
+        life--;
+        death();
     }
     document.getElementById(items[chosenItem]).style.display = "none";
     randomItem();
@@ -46,8 +57,12 @@ function checkCompost() {
 function checkRefundables() {
     if (chosenItem == 6 || chosenItem == 7 || chosenItem == 8) {
         console.log("DEVU IS PROUD");
+        score++;
     } else {
         console.log("GO HOME BRO");
+        score--;
+        life--;
+        death();
     }
     document.getElementById(items[chosenItem]).style.display = "none";
     randomItem();
@@ -56,11 +71,22 @@ function checkRefundables() {
 function checkMixedPaper() {
     if (chosenItem == 9 || chosenItem == 10) {
         console.log("WIN");
+        score++;
     } else {
         console.log("DIDN'T YOUR MAMMA TEACH YOU HOW TO RECYCLE???");
+        score--;
+        life--;
+        death();
     }
     document.getElementById(items[chosenItem]).style.display = "none";
     randomItem();
 }
 
+function death() {
+    if (life==0){
+        alert("GAME OVER!");
+    }
+}
+
 randomItem();
+
